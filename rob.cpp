e1be98962a418a872ea0e9c3fec18da648e6b6ee
chenglin.bi <bichenglin5980@gmail.com>
2022-02-16 17:49:13 +0800

    tab to space

--- rob.cpp
+++ rob.cpp
@@ -8,90 +8,89 @@
 void fillnop(unsigned char *instBuf, unsigned sizeBytes)
 {
 #ifdef __aarch64__
-	unsigned nopCnt = sizeBytes / 4;
-	unsigned* inst = (unsigned*)instBuf;
-	for (int i = 0; i < nopCnt; i++)
-		inst[i] = 0xd503201f;
+    unsigned nopCnt = sizeBytes / 4;
+    unsigned *inst = (unsigned *)instBuf;
+    for (int i = 0; i < nopCnt; i++)
+        inst[i] = 0xd503201f;
 #else
-	memset(instBuf, 0x90, sizeBytes);
+    memset(instBuf, 0x90, sizeBytes);
 #endif
 }
 
 void rob_test(unsigned char *instBuf, int nopCnt, int sqrtCnt)
 {
-	int i = 0;
+    int i = 0;
 #ifdef __aarch64__
-	// double d = (double)rand();
-	// d = sqrt(d);
+    // double d = (double)rand();
+    // d = sqrt(d);
 
-	// generate sqrt d0, d0
-	unsigned int* inst = (unsigned int*)instBuf;
-	for (int j = 0; j < sqrtCnt; j++)
-	{
-		// 1e61c001
-		inst[i++] = 0x1e61c000;
-	}
+    // generate sqrt d0, d0
+    unsigned int *inst = (unsigned int *)instBuf;
+    for (int j = 0; j < sqrtCnt; j++)
+    {
+        inst[i++] = 0x1e61c000;
+    }
 
-	// generate nop
-	for (int j = 0; j < nopCnt; j++)
-	{
-		inst[i++] = 0xd503201f;
-	}
-	
+    // generate nop
+    for (int j = 0; j < nopCnt; j++)
+    {
+        inst[i++] = 0xd503201f;
+    }
+
     // ret 0xd65f03c0
-	inst[i++] = 0xd65f03c0;
+    inst[i++] = 0xd65f03c0;
 #else
-	// generate sqrtsd %xmm0, %xmm0
-	for (int j = 0; j < sqrtCnt; j++)
-	{
-		instBuf[i++] = 0xf2;
-		instBuf[i++] = 0x0f;
-		instBuf[i++] = 0x51;
-		instBuf[i++] = 0xc0;
-	}
+    // generate sqrtsd %xmm0, %xmm0
+    for (int j = 0; j < sqrtCnt; j++)
+    {
+        instBuf[i++] = 0xf2;
+        instBuf[i++] = 0x0f;
+        instBuf[i++] = 0x51;
+        instBuf[i++] = 0xc0;
+    }
 
-	// generate nop
-	for (int j = 0; j < nopCnt; j++)
-	{
-		instBuf[i++] = 0x90;
-	}
-	// generate ret
-	instBuf[i++] = 0xc3;
+    // generate nop
+    for (int j = 0; j < nopCnt; j++)
+    {
+        instBuf[i++] = 0x90;
+    }
+    // generate ret
+    instBuf[i++] = 0xc3;
 #endif
 
-	// warm icache
-	((void (*)())instBuf)();
+    // warm icache
+    ((void (*)())instBuf)();
 
-	unsigned long long start = getclock();
-	const static int LoopCnt = 10000;
-	for (int i = 0; i < LoopCnt; i++)
-	{
-		((void (*)())instBuf)();
-	}
-	unsigned long long end = getclock();
-	unsigned long long clock = end - start;
-	printf("%lld, ", clock / LoopCnt);
+    unsigned long long start = getclock();
+    const static int LoopCnt = 10000;
+    for (int i = 0; i < LoopCnt; i++)
+    {
+        ((void (*)())instBuf)();
+    }
+    unsigned long long end = getclock();
+    unsigned long long clock = end - start;
+    printf("%lld, ", clock / LoopCnt);
 }
 
 int main(int argc, char **argv)
 {
-	SetProcessAffinityMask(GetCurrentProcess(), 0x1);
-	SetProcessPriorityBoost(GetCurrentProcess(), true);
-	SetPriorityClass(GetCurrentProcess(), REALTIME_PRIORITY_CLASS);
-	SetThreadPriority(GetCurrentThread(), THREAD_PRIORITY_TIME_CRITICAL);
-	unsigned char *code = (unsigned char *)VirtualAlloc(0, 0x101000, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
-	unsigned char *instBuf = (unsigned char*)((size_t)(code + 0xfff) & (~0xfff));
-	fillnop(instBuf, 0x100000);
+    SetProcessAffinityMask(GetCurrentProcess(), 0x1);
+    SetProcessPriorityBoost(GetCurrentProcess(), true);
+    SetPriorityClass(GetCurrentProcess(), REALTIME_PRIORITY_CLASS);
+    SetThreadPriority(GetCurrentThread(), THREAD_PRIORITY_TIME_CRITICAL);
+    unsigned char *code = (unsigned char *)VirtualAlloc(0, 0x101000, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+    unsigned char *instBuf = (unsigned char *)((size_t)(code + 0xfff) & (~0xfff));
+    fillnop(instBuf, 0x100000);
 
-	for (int nopCnt = 100; nopCnt < 1000; nopCnt += 10)
-	{
-		printf("%d, ", nopCnt);
-		for (int sqrtCnt = 3; sqrtCnt < 15; sqrtCnt++)
-		{
-			rob_test(instBuf, nopCnt, sqrtCnt);
-		}
-		printf("\n");
-	}
-	VirtualFree(code, 0x101000, MEM_RELEASE);
-	return 0;
+    for (int nopCnt = 100; nopCnt < 1000; nopCnt += 10)
+    {
+        printf("%d, ", nopCnt);
+        for (int sqrtCnt = 3; sqrtCnt < 15; sqrtCnt++)
+        {
+            rob_test(instBuf, nopCnt, sqrtCnt);
+        }
+        printf("\n");
+    }
+    VirtualFree(code, 0x101000, MEM_RELEASE);
+    return 0;
 }
